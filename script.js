7d3694266c73f5341ea052218bd9da6382417114
meshubhampol <maheshpol9@gmail.com>
2023-01-31 14:29:38 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your code here
+let input=document.getElementById('evaluatedText');
+input.addEventListener('keydown',updateCount);
+input.addEventListener('keyup',updateCount);
+
+let score=document.getElementById('wordCount');
+
+function updateCount() {
+    let s=input.value;
+    score.innerHTML=s.length;
+}
